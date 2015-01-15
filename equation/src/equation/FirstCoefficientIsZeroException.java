64d41e0f201465138428987ee1c6e24f943a1e35
susustudent <valentainmichaelsmit@gmail.com>
2015-01-16 04:48:28 +0500

    Revert "Revert "Revert "Initial commit"""
    This reverts commit 3b37f0bf2ed75b5e194ffb1aebb59760cc152d51.

--- equation/src/equation/FirstCoefficientIsZeroException.java
+++
@@ -1,7 +0,0 @@
-package equation;
-
-public class FirstCoefficientIsZeroException extends ArithmeticException
-{
-	FirstCoefficientIsZeroException(){};
-	FirstCoefficientIsZeroException(String msg){super(msg);};
-}
--- people/src/people/WrongAgeValueException.java
+++
@@ -1,6 +0,0 @@
-package people;
-
-public class WrongAgeValueException extends Exception
-{
-	WrongAgeValueException(){super("age of person is below than zero");};
-}
